ba7b9c6bf6be9348f2b5774305170cb2a49e71c2
deep23072000 <dp089901@gmail.com>
2024-10-08 19:29:43 +0530

    sidebar creation

---
+++ sidebar.js
@@ -0,0 +1,10 @@
+function showside(){
+    let selectaside = document.querySelector('.sidebar');
+    selectaside.style.marginLeft = "0px";
+    selectaside.style.transition = "margin-left .5s"
+
+    let selectsmallaside = document.querySelector('.smallsidebar');
+    selectsmallaside.style.display = "none";
+
+
+}
