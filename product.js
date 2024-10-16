9fa1f5fa9a877c1407d38f406e47cd020a344e3c
deep23072000 <dp089901@gmail.com>
2024-10-16 19:33:14 +0530

    addd files

--- product.js
+++ product.js
@@ -25,6 +25,66 @@ function submitdata(){
     // window.location.href = "./index.html"
 }
 
+// =======================================delete data==========================
+
+function del(arg){
+    
+    let con = window.confirm("Do you want to delete product information");
+    if(con){
+        fetch(`http://localhost:4000/products/${arg}`,{
+            method:"DELETE"
+        })
+    }else{
+        alert("invalid click")
+    }
+   
+}
+
+// ====================================open form=========================
+let uid = null;
+async function openform(arg){
+    uid= arg
+    let data = await fetch('http://localhost:4000/products');
+    let response = await data.json();
+
+    let info = response.find(e=>e.id === arg);
+    console.log(info)
+
+    let selectform = document.querySelector('.productform');
+    selectform.style.display = "block";
+    let selectubtn = document.querySelector('.ubtn');
+    selectubtn.style.display = "block";
+    let selectpbtn = document.querySelector('.pbtn');
+    selectpbtn.style.display = "none";
+
+
+    document.querySelector('#name').value = info.name;
+    document.querySelector('#email').value = info.email;
+}
+// ========================================================================
+// ========================final update================
+
+function finalupdate(){
+    
+    let name = document.querySelector('#name').value;
+    
+    let email = document.querySelector('#email').value;
+    let obj = {
+        "name":name,
+        "email":email
+    }
+   
+    fetch(`http://localhost:4000/products/${uid}`,{
+        method:"PUT",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(obj)
+    })
+
+    alert("right code");
+    return false;
+}
 (async function(){
     let data = await fetch('http://localhost:4000/products');
     let response = await data.json();
@@ -35,6 +95,8 @@ function submitdata(){
     <td> ${items.id}</td>
     <td> ${items.name}</td>
     <td> ${items.email}</td>
+    <td onclick="del('${items.id}')"> <i class="fa-solid fa-trash"></i></td>
+    <td onclick="openform('${items.id}')"> <i class="fa-solid fa-pen-to-square"></i></td>
     </tr>
     `).join(" ")
 })()
